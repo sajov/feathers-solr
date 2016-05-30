03c45840c88feafe634c559642b82633c4240f06
Sascha Jovanoski <info@sajo-media.de>
2016-05-31 01:43:09 +0200

    add solr version 6.0.0

--- bin/install-solr.sh
+++ bin/install-solr.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-FILE="solr-6.0.1.tgz"
-URL="https://archive.apache.org/dist/lucene/solr/6.0.1/"
+FILE="solr-6.0.0.tgz"
+URL="https://archive.apache.org/dist/lucene/solr/6.0.0/"
 
 wget $URL$FILE
-tar xfz solr-6.0.1.tgz
-cd solr-6.0.1
+tar xfz solr-6.0.0.tgz
+cd solr-6.0.0
 bin/solr start -e schemaless
