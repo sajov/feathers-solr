0684ee152a6b56de3e49df8a604222d28fc2c08a
Sascha Jovanoski <info@sajo-media.de>
2017-06-20 22:01:25 +0200

    Add solr 6.6.0 kickstart

--- bin/install-solr.sh
+++ bin/install-solr.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-FILE="solr-6.0.1.tgz"
-URL="https://archive.apache.org/dist/lucene/solr/6.0.1/"
+FILE="solr-6.6.0.tgz"
+URL="https://archive.apache.org/dist/lucene/solr/6.6.0/"
 
 wget $URL$FILE
-tar xfz solr-6.0.1.tgz
-cd solr-6.0.1
+tar xfz solr-6.6.0.tgz
+cd solr-6.6.0
 java -version
 bin/solr start -e schemaless
