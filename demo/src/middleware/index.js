5b097d4053eef72bbaad4ede5336de2d8edfa659
Sascha Jovanoski <info@sajo-media.de>
2017-07-11 01:54:12 +0200

    cleanup

--- demo/src/middleware/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = function () {
-  // Add your custom middleware here. Remember, that
-  // in Express the order matters
-  const app = this; // eslint-disable-line no-unused-vars
-};
--- demo/src/services/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = function () {
-  const app = this; // eslint-disable-line no-unused-vars
-};
