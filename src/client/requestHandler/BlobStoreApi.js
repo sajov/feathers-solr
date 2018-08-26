0292590553e1d9e9325e1643a12784acde721098
Sascha Jovanoski <info@sajo-media.de>
2018-08-26 16:46:24 +0200

    utils test

--- src/client/requestHandler/BlobStoreApi.js
+++ src/client/requestHandler/BlobStoreApi.js
@@ -2,8 +2,4 @@
  * Solr BolbStore API
  * https://cwiki.apache.org/confluence/display/solr/Blob+Store+API
  */
-export function someMethod() {
-    console.log('someMethod');
-}
-
-export var another = {};
+export function BolbStore() {}
