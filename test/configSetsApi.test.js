40bce43a943f3b46ba1cb97106b5de16d90cef29
Sascha Jovanoski <info@sajo-media.de>
2018-08-29 18:57:16 +0200

    no message

--- test/configSetsApi.test.js
+++ test/configSetsApi.test.js
@@ -1,82 +1,90 @@
-// import { expect } from 'chai';
-// import plugin from '../src';
+import { expect } from 'chai';
+import plugin from '../src';
 
-// let Adapter = new plugin.Service({
-//   paginate: {
-//     default: 10,
-//     max: 4
-//   }
-// });
-// describe('ConfigSetApi', () => {
-
-//   describe('List', () => {
-//     it('contains configSet `default`', done => {
-//       Adapter.client().configSets().list()
-//             .then(function(res) {
-//                 expect(res.responseHeader.status).to.be.equal(0);
-//                 expect(res.configSets).to.be.an('array').that.does.include('_default');
-//                 done();
-//             })
-//             .catch(function(err) {
-//                 done(err);
-//             });
-//     });
-//   });
-
-
-//   describe('Create', () => {
-//     it('create configSet `sajov`', done => {
-//       Adapter.client()
-//             .configSets()
-//             .create({name:'sajov'})
-//             .then(function(res) {
-//                 expect(res.responseHeader.status).to.be.equal(0);
-//                 done();
-//             })
-//             .catch(function(err) {
-//                 done(err);
-//             });
-//     });
-
-//     it('list configSet contains `sajov`', done => {
-//       Adapter.client().configSets().list()
-//             .then(function(res) {
-//                 expect(res.responseHeader.status).to.be.equal(0);
-//                 expect(res.configSets).to.be.an('array').that.does.include('sajov');
-//                 done();
-//             })
-//             .catch(function(err) {
-//                 done(err);
-//             });
-//     });
-//   });
+let Adapter = new plugin.Service({
+  paginate: {
+    default: 10,
+    max: 4
+  }
+});
+describe('ConfigSetApi', function()  {
+  this.timeout(10000);
+  describe('List', function()  {
+    this.timeout(10000);
+    it('contains configSet `default`', function(done) {
+      this.timeout(10000);
+      Adapter.client().configSets().list()
+            .then(function(res) {
+                expect(res.responseHeader.status).to.be.equal(0);
+                expect(res.configSets).to.be.an('array').that.does.include('_default');
+                done();
+            })
+            .catch(function(err) {
+                done(err);
+            });
+    });
+  });
 
 
-//   describe('Delete', () => {
-//     it('delete configSet `sajov`', done => {
-//       Adapter.client()
-//             .configSets()
-//             .delete({name:'sajov'})
-//             .then(function(res) {
-//                 expect(res.responseHeader.status).to.be.equal(0);
-//                 done();
-//             })
-//             .catch(function(err) {
-//                 done(err);
-//             });
-//     });
+  describe('Create', function()  {
+    this.timeout(10000);
+    it('create configSet `sajov`', function(done) {
+      this.timeout(10000);
+      Adapter.client()
+            .configSets()
+            .create({name:'sajov'})
+            .then(function(res) {
+                expect(res.responseHeader.status).to.be.equal(0);
+                done();
+            })
+            .catch(function(err) {
+                done(err);
+            });
+    });
 
-//     it('not contains configSet `sajov`', done => {
-//       Adapter.client().configSets().list()
-//             .then(function(res) {
-//                 expect(res.responseHeader.status).to.be.equal(0);
-//                 expect(res.configSets).to.be.an('array').that.does.not.include('sajov');
-//                 done();
-//             })
-//             .catch(function(err) {
-//                 done(err);
-//             });
-//     });
-//   });
+    it('list configSet contains `sajov`', function(done) {
+      this.timeout(10000);
+      Adapter.client().configSets().list()
+            .then(function(res) {
+                expect(res.responseHeader.status).to.be.equal(0);
+                expect(res.configSets).to.be.an('array').that.does.include('sajov');
+                done();
+            })
+            .catch(function(err) {
+                done(err);
+            });
+    });
+  });
 
-// });
+
+  describe('Delete', function()  {
+    this.timeout(10000);
+    it('delete configSet `sajov`', function(done) {
+      this.timeout(10000);
+      Adapter.client()
+            .configSets()
+            .delete({name:'sajov'})
+            .then(function(res) {
+                expect(res.responseHeader.status).to.be.equal(0);
+                done();
+            })
+            .catch(function(err) {
+                done(err);
+            });
+    });
+
+    it('not contains configSet `sajov`', function(done) {
+      this.timeout(10000);
+      Adapter.client().configSets().list()
+            .then(function(res) {
+                expect(res.responseHeader.status).to.be.equal(0);
+                expect(res.configSets).to.be.an('array').that.does.not.include('sajov');
+                done();
+            })
+            .catch(function(err) {
+                done(err);
+            });
+    });
+  });
+
+});
