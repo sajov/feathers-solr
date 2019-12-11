2036a5885b63b9446e39689f8f7bf142afc7c299
Sascha Jovanoski <s.jovanoski@reuter.de>
2019-12-11 11:30:11 +0100

    Refactor
    Upgarde trvis test solr 7.7.2
    
    Skipped the following 25 Feathers adapter test(s) out of 62 total:
    [
      ".events",
      "._patch",
      ".get + id + query",
      ".get + NotFound",
      ".get + id + query id",
      ".remove",
      ".remove + $select",
      ".remove + id + query",
      ".remove + multi",
      ".remove + id + query id",
      ".update",
      ".update + $select",
      ".update + id + query",
      ".update + NotFound",
      ".update + id + query id",
      ".patch",
      ".patch + $select",
      ".patch + id + query",
      ".patch multiple",
      ".patch multi query",
      ".patch + NotFound",
      ".patch + id + query id",
      ".create",
      ".create + $select",
      ".find + $or nested + $sort"
    ]
    
      40 passing (604ms)
      25 pending

--- bin/install-solr.sh
+++ bin/install-solr.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-FILE="solr-7.4.0.tgz"
-URL="https://archive.apache.org/dist/lucene/solr/7.4.0/"
+FILE="solr-7.7.2.tgz"
+URL="https://archive.apache.org/dist/lucene/solr/7.7.2/"
 
 
 wget $URL$FILE
-tar xfz solr-7.4.0.tgz
-cd solr-7.4.0
+tar xfz solr-7.7.2.tgz
+cd solr-7.7.2
 
 java -version
 #bin/solr start -e schemaless
