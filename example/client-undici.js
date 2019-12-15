6e7202d2978e9ddea7fc48e156caecab4737729d
sajov <info@sajo-media.de>
2019-12-15 17:22:00 +0100

    Refactor
    Benchmark
    - fetch
    - fast-req
    - undici
    
    Skipped the following 1 Feathers adapter test(s) out of 62 total:
    [
      ".find + $or nested + $sort"
    ]
    
      72 passing (6s)
      1 pending

--- example/client-undici.js
+++ example/client-undici.js
@@ -12,12 +12,10 @@ class HttpClient {
 
     this.conn = conn;
     this.params = { wt: "json" };
-
     this.client = new Client(`http://localhost:8983`, {
       connections: 100,
       pipelining: 10
     });
-    // console.log("this.client", this.client);
   }
 
   get(api, params = {}) {
@@ -25,44 +23,31 @@ class HttpClient {
       Object.assign({}, params, this.params),
       { encode: false }
     )}`;
-
-    debug("GET", url);
-
-    const options = {
-      url: url,
-      method: "get",
-      "content-type": "application/json"
-    };
+    const self = this;
     return new Promise(function(resolve, reject) {
-      req(options, (err, response) => {
-        // console.log("FAST REQUEST GET", err, response.body);
-        if (err) reject(err);
-        return resolve(response.body);
-      });
+      self.client.request(
+        {
+          headers: {
+            "content-type": "application/json"
+          },
+          path: `/solr/techproducts/${api}?${qs.stringify(
+            Object.assign({}, params, this.params),
+            { encode: false }
+          )}`,
+          method: "GET"
+        },
+        function(err, data) {
+          if (err) reject(err);
+          const { statusCode, headers, body } = data;
+          body.on("data", d => {
+            resolve(JSON.parse(d.toString()));
+          });
+        }
+      );
     });
   }
 
   async post(api, data, params = {}) {
-    // const z = `http://localhost:8983/solr/techproducts/${api}`;
-    // console.log(z, data);
-    // const { statusCode, headers, body } = await this.client.request({
-    //   headers: {
-    //     "content-type": "application/json"
-    //   },
-    //   path: `/solr/techproducts/${api}`,
-    //   method: "POST",
-    //   // body: data
-    //   // body: JSON.stringify(data)
-    //   body: Buffer.from(JSON.stringify(data))
-    // });
-
-    // console.log("response received", statusCode);
-    // console.log("headers", headers);
-    // body.on("data", d => {
-    //   console.log(JSON.parse(d.toString()));
-    //   return JSON.parse(d.toString());
-    // });
-
     const self = this;
     return new Promise(function(resolve, reject) {
       self.client.request(
@@ -77,13 +62,9 @@ class HttpClient {
         function(err, data) {
           if (err) reject(err);
           const { statusCode, headers, body } = data;
-          // console.log("response received", statusCode);
-          // console.log("headers", headers);
           body.on("data", d => {
             resolve(JSON.parse(d.toString()));
           });
-
-          // this.client.close();
         }
       );
     });
