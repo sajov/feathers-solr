ba3b02d06061b4a9a094623c0d1f015bd375176b
sajov <info@sajo-media.de>
2021-11-14 18:20:50 +0100

    task: remove lib

--- lib/client.d.ts
+++
@@ -1,9 +0,0 @@
-export interface SolrClient {
-    get: (resource: string, params?: any) => Promise<any>;
-    post: (resource: string, params?: any, data?: any) => Promise<any>;
-}
-export interface SolrClientOptions {
-    host: string;
-    core: string;
-}
-export declare const solrClient: (options: SolrClientOptions) => SolrClient;
--- lib/response.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Response parser
- */
-export declare function responseFind(filters: any, query: any, paginate: any, res: any): any;
-/**
- * Response Docs parser
- */
-export declare function responseGet(res: any, allDocs?: boolean): any;
