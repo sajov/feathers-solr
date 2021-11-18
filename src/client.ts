c0abe03414397164c9e63168658f8a5b5f3025d6
sajov <info@sajo-media.de>
2021-11-18 21:57:26 +0100

    fix schema seed

--- src/client.ts
+++ src/client.ts
@@ -29,7 +29,6 @@ const request = async (options: RequestOptions) => {
   const { method } = requestOptions;
   const { protocol } = new URL(url);
   const transport = protocol === 'https:' ? https : http;
-  console.log(options);
 
   return new Promise((resolve, reject) => {
     const request = transport.request(
