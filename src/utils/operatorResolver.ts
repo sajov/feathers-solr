c78cf723376aec303bd96b2bce21d3648b546795
Sascha Jovanoski <s.jovanoski@reuter.de>
2022-12-11 13:26:11 +0100

    chore: cleanup operatorResolver

--- src/utils/operatorResolver.ts
+++ src/utils/operatorResolver.ts
@@ -1,14 +1,14 @@
 export const operatorResolver: any = {
+  $eq: (key: string, value: any) => `${key}:${value}`,
+  $ne: (key: string, value: any) => `!${key}:${value}`,
+  $like: (key: string, value: any) => `${key}:*${value}*`,
+  $nlike: (key: string, value: any) => `!${key}:*${value}*`,
   $in: (key: string, value: any) => `${key}:(${value.join(' OR ')})`,
+  $nin: (key: string, value: any) => `!${key}:(${value.join(' OR ')})`,
+  $and: (value: any) => `(${value.join(' AND ')})`,
   $or: (value: any) => `(${value.join(' OR ')})`,
   $lt: (key: string, value: any) => `${key}:[* TO ${value}}`,
   $lte: (key: string, value: any) => `${key}:[* TO ${value}]`,
   $gt: (key: string, value: any) => `${key}:{${value} TO *]`,
-  $gte: (key: string, value: any) => `${key}:[${value} TO *]`,
-  $like: (key: string, value: any) => `${key}:*${value}*`,
-  $nlike: (key: string, value: any) => `!${key}:*${value}*`,
-  $eq: (key: string, value: any) => `${key}:${value}`,
-  $ne: (key: string, value: any) => `!${key}:${value}`,
-  $and: (value: any) => `(${value.join(' AND ')})`,
-  $nin: (key: string, value: any) => `!${key}:(${value.join(' OR ')})`
+  $gte: (key: string, value: any) => `${key}:[${value} TO *]`
 }
