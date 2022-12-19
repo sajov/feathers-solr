8c4b3651aa9fe6aab80977e0033b10100df3d9d9
Sascha Jovanoski <s.jovanoski@reuter.de>
2022-12-19 17:21:37 +0100

    task: cleanups

--- src/utils/convertOperators.ts
+++ src/utils/convertOperators.ts
@@ -4,11 +4,12 @@ import { _ } from '@feathersjs/commons/lib';
 export function convertOperators(query: any, escapeFn: any, root = ''): any {
   if (Array.isArray(query)) return query.map(q => convertOperators(q, escapeFn));
 
-  return Object.keys(query).map(prop =>  prop === '$or' ?
-          operatorResolver.$or(convertOperators(query[prop], escapeFn)) :
-          typeof operatorResolver[prop] !== 'undefined' ?
-            operatorResolver[prop](...Object.values(escapeFn(root, query[prop]))) :
-            _.isObject(query[prop]) ?
-              operatorResolver.$and(convertOperators(query[prop], escapeFn, prop)) :
-              operatorResolver.$eq(...Object.values(escapeFn(prop, query[prop]))));
+  return Object.keys(query).map((prop: any) =>
+    prop === '$or' ?
+      operatorResolver.$or(convertOperators(query[prop], escapeFn)) :
+      typeof operatorResolver[prop] !== 'undefined' ?
+        operatorResolver[prop](...Object.values(escapeFn(root, query[prop]))) :
+        _.isObject(query[prop]) ?
+          operatorResolver.$and(convertOperators(query[prop], escapeFn, prop)) :
+          operatorResolver.$eq(...Object.values(escapeFn(prop, query[prop]))));
 }
