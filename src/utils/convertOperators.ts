c5c5eb814a68a21e70382b3b14462161c9a00607
Sascha Jovanoski <s.jovanoski@reuter.de>
2022-12-19 17:26:24 +0100

    task: cleanups

--- src/utils/convertOperators.ts
+++ src/utils/convertOperators.ts
@@ -1,15 +1,14 @@
 import { operatorResolver } from './operatorResolver';
 import { _ } from '@feathersjs/commons/lib';
 
-export function convertOperators(query: any, escapeFn: any, root = ''): any {
-  if (Array.isArray(query)) return query.map(q => convertOperators(q, escapeFn));
-
-  return Object.keys(query).map((prop: any) =>
-    prop === '$or' ?
-      operatorResolver.$or(convertOperators(query[prop], escapeFn)) :
-      typeof operatorResolver[prop] !== 'undefined' ?
-        operatorResolver[prop](...Object.values(escapeFn(root, query[prop]))) :
-        _.isObject(query[prop]) ?
-          operatorResolver.$and(convertOperators(query[prop], escapeFn, prop)) :
-          operatorResolver.$eq(...Object.values(escapeFn(prop, query[prop]))));
-}
+export const convertOperators = (query: any, escapeFn: any, root = ''): string[] =>
+  Array.isArray(query) ?
+    query.map(q => convertOperators(q, escapeFn)) :
+    Object.keys(query).map((prop: any) =>
+      prop === '$or' ?
+        operatorResolver.$or(convertOperators(query[prop], escapeFn)) :
+        typeof operatorResolver[prop] !== 'undefined' ?
+          operatorResolver[prop](...Object.values(escapeFn(root, query[prop]))) :
+          _.isObject(query[prop]) ?
+            operatorResolver.$and(convertOperators(query[prop], escapeFn, prop)) :
+            operatorResolver.$eq(...Object.values(escapeFn(prop, query[prop]))));
